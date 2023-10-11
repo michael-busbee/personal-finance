41c1929467b6c88ad94ec13c44d95f7c5890464e
Michael Busbee <plots_wispy.0c@icloud.com>
2023-10-11 19:52:43 -0400

    Updated /envelopes/:envelopeName path to give error message for invalid envelope name

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 4001;
 
 const envelopes = {
-
     Housing: 1200,
-    Utilities: 210,
-    Groceries: 400,
-    Gas: 60,
-
+    Utilities: 400,
+    Groceries: 400
 };
 
 app.get('/envelopes', (req, res, next) => {
@@ -19,7 +16,17 @@ app.get('/envelopes', (req, res, next) => {
 
 app.get('/envelopes/:envelopeName', (req, res, next) => {
     const envelopeName = req.params.envelopeName;
+    if(!envelopes[envelopeName]){
+        res.send("Envelope not found! Please check your spelling and try again.")
+    }
     const message = `${envelopeName}: ${envelopes[envelopeName]}`;
+    res.send(message);
+});
+
+app.post('/envelopes/:envelopeName/:debit', (req, res, next) => {
+    const debit = req.params.debit;
+    const envelopeName = req.params.envelopeName;
+    const message = `${envelopeName}: ${debit}`;
     res.send(message);
 });
 
